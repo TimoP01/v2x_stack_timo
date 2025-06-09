f483bb27c3892b129e4a6569a8a79001cc2197b3
MaxxBahr <schnee.bahr@googlemail.com>
2025-06-09 15:05:47 +0000

    added vam_rx

--- src/vam_rx.hpp
+++ src/vam_rx.hpp
@@ -9,20 +9,20 @@
 namespace v2x_stack_btp
 {
 
-class VaRxNode : public rclcpp::Node
+class CaRxNode : public rclcpp::Node
 {
 public:
-    explicit VaRxNode(const rclcpp::NodeOptions & options);
+    explicit CaRxNode(const rclcpp::NodeOptions & options);
     void onIndication(const msg::BtpDataIndication::ConstSharedPtr);
 
 private:
     
-    void publish(const vanetza::asn1::r1::Vam);
+    void publish(const vanetza::asn1::r1::Cam);
 
     uint16_t port_;
     rclcpp::Subscription<msg::BtpDataIndication>::SharedPtr sub_btp_;
     //rclcpp::Publisher<ros_etsi_its_msgs::msg::CAM>pub_cam_;
-    std::shared_ptr<rclcpp::Publisher<ros_etsi_its_msgs::msg::VAM>> pub_vam_; // Where is the constructor for VAM????
+    std::shared_ptr<rclcpp::Publisher<ros_etsi_its_msgs::msg::CAM>> pub_cam_;
     rclcpp::Node::SharedPtr node_;
 };
 
